3def3fc8ed4cae65348b48271654af4cb4c9094a
mahisa agnu <54902186+mahisa23-siks@users.noreply.github.com>
2019-09-14 19:00:34 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+## INSTAGRAM TOOLS [TOOLSIG]
+* Instagram Tools Based on NodeJS (This script or code was created by CrossX - mahisa23).
+* Tools (BomLikeTarget,Botlike1,Botlike,Fah,Fftauto,Flaauto,Flmauto,Unfollall,Unfollnotfollback).
+
+## Tutorial on Termux [ANDROID]
+	$ pkg install git
+	$ pkg install nodejs-lts
+	$ git clone https://github.com/mahisa23-siks/botig && cd index.js
+	$ unzip node_modules.zip
+	$ node index.js
+
+	
+## License
+	☑ This project is licensed under MIT License 2017-2019. https://opensource.org/licenses/MIT.
---
+++ index.js
@@ -0,0 +1,98 @@
+﻿'use strict'
+
+// code by : @mahisa23
+// ig : @mahisa23
+// fb : @mahisa23
+// websitte : www.siksonline.com and www.hokilagi.com
+
+const Client = require('instagram-private-api').V1; 
+const delay = require('delay');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+var moment = require("moment"); //DETECT CALENDER
+var colors = require('colors'); //DETECT COLORS
+var userHome = require('user-home'); //DETECT USER PC/HOME
+
+//DETECT IP *START!
+var os = require('os');
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === 'IPv4' && !address.internal) {
+            addresses.push(address.address);
+        }
+    }
+}
+//DETECT IP *END!
+
+const questionTools = [
+{
+  type:"list",
+  name:"Tools",
+  message:"Select tools:\n ",
+  choices:
+  [
+  "► Bom Like Target              [USING ITTYW/DELAY]",
+  "► Unfollow All Following       [USING ITTYW/DELAY]",
+  "► Unfollow Not Followback      [USING ITTYW/DELAY]",
+  "► Follow Followers Target      [USING ITTYW/DELAY]",
+  "\n"
+  ] 
+}
+]
+const main = async () => {
+  //created by mahisa23 (@CrossX)
+  try{
+    var toolChoise = await inquirer.prompt(questionTools);
+    toolChoise = toolChoise.Tools;
+    switch(toolChoise){
+
+      case "► Bom Like Target              [USING ITTYW/DELAY]":
+      const bomliketarget = require('./tools/bomliketarget.js');
+      await bomliketarget();
+      break;
+
+      case "► Mass Delete Post/Photo       [USING ITTYW/DELAY]":
+      const dellallphoto = require('./tools/dellallphoto.js');
+      await dellallphoto();
+      break;
+
+      case "► Unfollow All Following       [USING ITTYW/DELAY]":
+      const unfollall = require('./tools/unfollall.js');
+      await unfollall();
+      break;
+
+      case "► Unfollow Not Followback      [USING ITTYW/DELAY]":
+      const unfollnotfollback = require('./tools/unfollnotfollback.js');
+      await unfollnotfollback();
+      break;
+
+      case "► Follow Followers Target      [USING ITTYW/DELAY]":
+      const fftauto = require('./tools/fftauto.js');
+      await fftauto();
+      break;
+
+      default:
+      console.log("\n ERROR:".red.bold,"Aw, Snap! Something went wrong while displaying this tool!\n".green.bold,"NOT FOUND! Please try again!".yellow.bold);
+    }
+  } catch(e) {
+    }
+  }
+
+  console.log(chalk`{bold.green
+  Ξ TITLE  : BOT INSTAGRAM
+  Ξ UPLOAD : 29/08/2019 [16.28 WIB]
+  Ξ CODEBY : mahisa23 [CrossX]
+  }`);
+  console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
+  console.log("  Ξ START  : ".bold.red + moment().format('D MMMM YYYY, h:mm:ss a'));
+  console.log("  Ξ YPATH  : ".bold.red +userHome);
+  console.log("  Ξ YOUIP  : ".bold.red +addresses);
+  console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
+  console.log(chalk`{bold.yellow
+  Ξ THANKS : BOT INSTAGRAM [Original Source File]
+           : mahisa23-siks}`);
+  console.log('\n')
+  main();
